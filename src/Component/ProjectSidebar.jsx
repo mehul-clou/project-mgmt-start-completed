d96eda0ba8ac4b4171f32fa201b1150aadcfe57d
CodeSandbox <support@codesandbox.io>
2024-01-14 10:11:13 +0000

    Initial commit

---
+++ src/Component/ProjectSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
