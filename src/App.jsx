3672c4eeed65e86f58074967462f1329fbe90847
mj <85698909+mehul-clou@users.noreply.github.com>
2024-01-14 10:58:42 +0000

    Update App.jsx and ProjectSidebar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProjectSidebar from "./Component/ProjectSidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8">
       <ProjectSidebar />
     </main>
   );
