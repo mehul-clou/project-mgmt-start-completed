67c571b5e125be0d4b367a305ff07d9576121b83
mj <85698909+mehul-clou@users.noreply.github.com>
2024-01-18 15:19:03 +0000

    Create Modal Component

--- src/Component/NewProject.jsx
+++ src/Component/NewProject.jsx
@@ -1,7 +1,9 @@
 import Input from "./Input.jsx";
 import { useRef } from "react";
+import Modal from "./Modal.jsx";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
   const title = useRef();
   const descrption = useRef();
   const dueDate = useRef();
@@ -16,7 +18,7 @@ export default function NewProject({ onAdd }) {
       enteredDescription.trim() === "" ||
       enteredDueDate.trim() === ""
     ) {
-      //Show error
+      //
     }
     onAdd({
       title: enteredTitle,
@@ -25,27 +27,30 @@ export default function NewProject({ onAdd }) {
     });
   }
   return (
-    <div className="wd[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={descrption} label="Discription" textarea />
-        <Input type="Date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} />
+      <div className="wd[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={descrption} label="Discription" textarea />
+          <Input type="Date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
